5f20002640be7539373f670ddf163daa9136b4ca
RiaChan2k <83418086+RiaChan2k@users.noreply.github.com>
2021-05-01 12:07:07 -0400

    add 3&4

--- Database.h
+++ Database.h
@@ -5,3 +5,5 @@ class Stock;
 int CreatePairTable(sqlite3*& db);
 int PopulatePairTable(sqlite3*& db);
 int InsertTable(sqlite3* db, const Stock& stock, std::string TableName);
+int CreatePairPricesTable(sqlite3*& db);
+int CalVol(sqlite3*& db);
